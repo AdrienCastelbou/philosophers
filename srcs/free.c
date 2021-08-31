c0029c637d1a64052c28856f774dab2358a8438a
acastelb <acastelb@student.42.fr>
2021-08-31 10:13:06 +0200

    mutex_destroy ok

--- srcs/free.c
+++ srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: acastelb <acastelb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 10:29:54 by acastelb          #+#    #+#             */
-/*   Updated: 2021/08/30 16:12:57 by user42           ###   ########.fr       */
+/*   Updated: 2021/08/31 10:11:16 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,25 @@ void	free_philos(t_philo *philo, int philos_nb)
 	t_philo	*next;
 	int		i;
 
-	i = 0;
-	while (i < philos_nb && philo)
+	if (!philo)
+		return ;
+	pthread_mutex_destroy(philo->write);
+	free(philo->write);
+	free(philo->finish);
+	free(philo->must_eat);
+	free(philo->t_start);
+	pthread_mutex_destroy(philo->check_end);
+	free(philo->check_end);
+	i = -1;
+	while (++i < philos_nb && philo)
 	{
 		next = philo->next;
+		pthread_mutex_destroy(philo->r_fork);
 		free(philo->r_fork);
-		if (philo->id == 1)
-		{
-			free(philo->write);
-			free(philo->finish);
-			free(philo->must_eat);
-			free(philo->t_start);
-			free(philo->check_end);
-		}
 		pthread_mutex_destroy(&philo->check_death);
-		philo->r_fork = NULL;
 		free(philo);
 		philo = NULL;
 		philo = next;
-		i++;
 	}
 }
 
